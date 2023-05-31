3f43745e6cda6727a384a345c07a9705e11a27b1
Diego Varzim <diego.varzim@hotmail.com>
2023-05-31 09:00:41 -0300

    ok

---
+++ main20.py
@@ -0,0 +1,9 @@
+#  For Loop - Separando Strings
+
+
+# Modificar de FANTASTICO PARA F A N T A S T I C O 
+
+palavra = 'DEVELOPER DE SUCESSO!!!!'
+
+for spaco in palavra:
+    print(f' {spaco}', end='')
---
+++ main21.py
@@ -0,0 +1,15 @@
+#  For loop 
+
+#  Criar um retângulo de 6 x 6
+# outer e inner loop
+
+
+linhas = 6          
+colunas = 6
+simbolo = '@'      
+
+for l in range(linhas):
+    for c in range(colunas):
+        print(simbolo, end='')  
+
+    print()    
