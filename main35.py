95fe745cedd11fba33ec93b6d5d10852c25ae8e3
Diego Varzim <diego.varzim@hotmail.com>
2023-06-08 12:28:17 -0300

    ok

--- main35.py
+++ main35.py
@@ -1,2 +1,10 @@
+# Listas
+#  Armazenar mais de uma informação em variáveis.
+#  Manter a sequência dos dados em uma variável.
 #  Looping dentro de uma lista
+
+valores = [50, 80, 110, 150, 170]
+
+for x in valores:
+    print(f' O valor final do produto é de R$ {x}')
 
---
+++ main39.py
@@ -0,0 +1,14 @@
+#  Tuples
+
+# Armazenas mais de uma informação em variáveis
+#  Manter a sequência dos dados em uma variável
+#  Não podem ser alteradas (Immutable)
+#  Tuple - utiliza parênteses ()
+
+
+cores_list = ['amarelo', 'verde', 'azul', 'vermelho']
+cores_tuple = ('amarelo', 'verde', 'azul', 'vermelho')
+
+cores_list.append('Roxo')
+
+print(cores_list)
